e9d2b41aaccb854541c8ac5d69cbd8311a0e46c8
als-v <alisson.v3@hotmail.com>
2021-08-25 21:23:14 -0300

    finalizado projeto

--- Projeto/projeto.m
+++ Projeto/projeto.m
@@ -1,13 +1,13 @@
 pkg load image;
 
 # abrir a imagem
-imagem = imread('imagem.jpg');
+imagemEntrada = imread('imagem.jpg');
 
 # rgb para hsv
-imagem = im2double(rgb2hsv(imagem));
+imagemConvertida = im2double(rgb2hsv(imagemEntrada));
 
-# restrinjo ao 3 array
-imagem = imagem(:,:,3);
+# restrinjo a imagem
+imagem = imagemConvertida(:,:,3);
 
 # tamanho da imagem
 [M, N] = size(imagem);
@@ -19,6 +19,42 @@ imagemRapidaFourier = fft2(imagem, P, Q);
 imagemRapidaFourier = fftshift(imagemRapidaFourier);
 espectro = uint8(abs(imagemRapidaFourier));
 
+## para livrar o codigo de executar o filtro toda vez, salvei o resultado dessa 
+## interação como a imagem 'filtro.png', para usar novamente
+#d0 = 175;
+#filtro = zeros(P, Q);
+
+#for u = 1:P
+#  for v = 1:Q
+#    Duv = (((u - P/2)^2) + ((v - Q/2)^2))^(1/2);
+#    filtro(u, v) = e ^ ((-Duv^2)/(2*(d0^2)));
+#  endfor
+#endfor
+
+# filtro de passa baixa
+filtro = imread('filtro.png');
+filtro = im2double(filtro);
+
+# multiplicação do filtro com a transformacao rapida de Fourier
+imagemFiltro = imagemRapidaFourier .* filtro;
+imagemFiltro = fftshift(imagemFiltro);
+imagemFiltro = ifft2(imagemFiltro);
+imagemFiltro = real(imagemFiltro);
+
+# mostra apenas a parte superior esquerda
+imagemFinal = zeros(M, N);
+imagemFinal = imagemFiltro(1:M, 1:N);
+
+# imagem recebe o resultado
+imagemConvertida(:,:,3) = imagemFinal;
+imagemConvertida = hsv2rgb(imagemConvertida);
+
+# ajuste RGB para melhor contraste
+imagemConvertida = imadjust(imagemConvertida,[.03 .03 .03; .85 .9 .7],[]);
+
+# salvo a imagem
+imwrite(imagemConvertida, 'resultado.png');
+
 figure(1);
-imshow(espectro);
-title('Imagem da transformação rápida de Fourier.');
+imshow(imagemConvertida);
+title('Resultado final.');
